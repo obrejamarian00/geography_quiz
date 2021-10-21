b47a669920e314264e929d0788d64029eb189b98
obrejamarian00 <obrejamarian00@gmail.com>
2021-10-21 10:24:33 +0100

    I had loaded all the code

---
+++ php/action_page.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>
+            Do you know geography?
+        </title>
+        <script src="js/script.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+    
+        <?php
+        
+        ?>
+
+    </body>
+</html>
